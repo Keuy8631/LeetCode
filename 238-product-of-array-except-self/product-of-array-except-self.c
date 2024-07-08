c34e6529ee4a15cf5bc2c28c1ec81a271476f5b9
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-08 17:03:09 +0800

    Time: 132 ms (53.43%) | Memory: 25.4 MB (94.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -3,14 +3,15 @@
  */
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     int *ans = (int*)malloc(numsSize*sizeof(int));
-    int *left = (int*)malloc(numsSize*sizeof(int));
-    int *right = (int*)malloc(numsSize*sizeof(int));
     int n = numsSize;
-    left[0] = 1;
-    for(int i=1; i<n; i++) left[i] = left[i-1]*nums[i-1];
-    right[n-1] = 1;
-    for(int i=n-2; i>=0; i--) right[i] = right[i+1]*nums[i+1];
-    for(int i=0; i<n; i++) ans[i] = left[i]*right[i];
+    ans[0] = 1;
+    for(int i=1; i<n; i++) ans[i] = ans[i-1]*nums[i-1];
+    int right = 1;
+    for(int i=n-1; i>=0; i--){
+        ans[i] *= right;
+        right *= nums[i];
+    } 
+
     *returnSize = n;
 
     return ans;
