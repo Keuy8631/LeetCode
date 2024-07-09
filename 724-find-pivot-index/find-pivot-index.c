c636d4ea8286723c36d6906e3e8a5e7fac91c904
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-10 05:14:59 +0800

    Time: 13 ms (81.86%) | Memory: 6.5 MB (26.83%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.c
+++ 724-find-pivot-index/find-pivot-index.c
@@ -1,19 +1,12 @@
 int pivotIndex(int* nums, int numsSize) {
-    int *prev = (int*)calloc(numsSize, sizeof(int));
-    int *sub = (int*)calloc(numsSize, sizeof(int));
-    prev[0] = nums[0];
-    for(int i=1; i<numsSize; i++) prev[i] = prev[i-1] + nums[i];
-    sub[numsSize-1] = nums[numsSize-1];
-    for(int i=numsSize-2; i>=0; i--) sub[i] = sub[i+1] + nums[i];
+    int total = 0;
+    for(int i=0; i<numsSize; i++) total += nums[i];
 
-    for(int i=0; i<numsSize; i++){ 
-        if(prev[i]==sub[i]){
-            free(prev);
-            free(sub);
-            return i;
-        }
+    int left_sum = 0;
+    for(int i=0; i<numsSize; i++){
+        if(left_sum == total-left_sum-nums[i]) return i;
+        left_sum += nums[i];
     }
-    free(prev);
-    free(sub);
+
     return -1;
 }
