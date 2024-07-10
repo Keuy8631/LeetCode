01ce404dede604bd1ec99ac4a5ca148a1092a2dc
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-10 21:04:32 +0800

    Time: 158 ms (53.97%) | Memory: 9.5 MB (99.21%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.c
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.c
@@ -1,24 +1,18 @@
 int equalPairs(int** grid, int gridSize, int* gridColSize) {
-    int row = gridSize, col = *gridColSize;
-    if(row!=col) return 0;
-
-    int col_table[row][col];
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-            col_table[i][j] = grid[j][i];
-        }
-    }
-
-    // grid[i][k] col_table[j][k]
     int cnt = 0;
+    int row = gridSize, col = *gridColSize;
+
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
+            bool equal = true;
             for(int k=0; k<col; k++){
-                if(grid[i][k]==col_table[j][k] && k==col-1) cnt++;
-                if(grid[i][k]!=col_table[j][k]) break;
+                if(grid[i][k]!=grid[k][j]){
+                    equal = false;
+                    break;
+                }
             }
+            if(equal) cnt++;
         }
     }
-
     return cnt;
 }
