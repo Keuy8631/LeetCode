58418e3b9c440a0aff0b049e533b2c52b452ab9c
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-12 16:35:10 +0800

    Time: 4 ms (50.49%) | Memory: 11.4 MB (6.10%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
@@ -10,7 +10,6 @@ int maxDepth(struct TreeNode* root) {
     if(!root) return 0;
     int left = maxDepth(root->left);
     int right = maxDepth(root->right);
-    printf("left=%d right=%d\n", left, right);
     if(left>right) return left+1;
     else return right+1;
 }
