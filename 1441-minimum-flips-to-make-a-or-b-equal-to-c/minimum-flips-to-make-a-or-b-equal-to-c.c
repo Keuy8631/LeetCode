0350d0b64785e6f2623f2af80601dc3b5b0b89d3
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-15 03:31:18 +0800

    Time: 0 ms (100.00%) | Memory: 7.7 MB (32.69%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.c
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.c
@@ -1,16 +1,12 @@
+int minFlips(int a, int b, int c) {
+    int cnt = 0;
+    for(int bit_pos=0; bit_pos<32; ++bit_pos){
+        bool a_bit = (a >> bit_pos) & 1;
+        bool b_bit = (b >> bit_pos) & 1;
+        bool c_bit = (c >> bit_pos) & 1;
 
-
-int minFlips(int a, int b, int c){
-    int cnt = 0, a_tmp = 0, b_tmp = 0, c_tmp = 0;
-    while(a||b||c){
-        a_tmp = a&1;
-        b_tmp = b&1;
-        c_tmp = c&1;
-        if(!c_tmp) cnt+=a_tmp+b_tmp;
-        else if(!a_tmp && !b_tmp) cnt += 1;
-        a>>=1;
-        b>>=1;
-        c>>=1;
+        if(!c_bit) cnt += a_bit + b_bit;
+        else if(!a_bit && !b_bit) cnt += 1;
     }
     return cnt;
 }
