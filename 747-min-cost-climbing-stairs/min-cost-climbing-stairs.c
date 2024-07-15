f45e503a92324fa0a5fd2898cfd5b6932815785f
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-15 23:28:16 +0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (5.95%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.c
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.c
@@ -1,12 +1,9 @@
-int compare(int a, int b){
-    if(a>b) return b;
-    else return a;
-}
+#define compare(a, b) ((a)<(b)?(a):(b))
 
 int minCostClimbingStairs(int* cost, int costSize) {
     if(!costSize) return 0;
     if(costSize==1) return cost[0];
-    int* dp = (int*)calloc(costSize, sizeof(int));
+    int dp[costSize];
     dp[0] = cost[0], dp[1] = cost[1];
     for(int i=2; i<costSize; i++)
         dp[i] = cost[i] + compare(dp[i-2], dp[i-1]);
