3f6fb0a2ae57e6066c7b3bb98b3137e4e350663c
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-15 20:35:15 +0800

    Time: 3 ms (29.03%) | Memory: 7.6 MB (12.90%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.c
@@ -0,0 +1,9 @@
+#define MOD 1000000007
+
+int numTilings(int n) {
+    unsigned int dp[n+3];
+    dp[0] = 1, dp[1] = 2, dp[2] = 5;
+    for(int i=3; i<n; i++)
+        dp[i] = (2*dp[i-1]+dp[i-3]) % MOD;
+    return dp[n-1];
+}
