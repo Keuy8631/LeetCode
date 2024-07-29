cf7b34ef2646379521df719cc87bb57e25832b4b
Keyu <74547765+Keuy8631@users.noreply.github.com>
2024-07-29 14:50:06 +0800

    Time: 74 ms (46.04%) | Memory: 19.8 MB (6.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,21 +1,7 @@
-void swap(int *a, int *b){
-    *a^=*b;
-    *b^=*a;
-    *a^=*b;
-}
-
 void moveZeroes(int* nums, int numsSize) {
-    if(numsSize<=1) return;
-    for(int i=0; i<numsSize-1; i++){
-        int ptr = i+1;
-        if(!nums[i]){
-            while(ptr<numsSize){
-                if(nums[ptr]){
-                    swap(&nums[i], &nums[ptr]);
-                    break;
-                }
-                else ptr++;
-            }
-        }
+    int ptr=0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i]) nums[ptr++] = nums[i];
     }
+    while(ptr<numsSize) nums[ptr++] = 0;
 }
