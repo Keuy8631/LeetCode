9c0c30685e254bd4b4e9c9b1308eb5e294436fb1
Keyu <74547765+Keuy8631@users.noreply.github.com>
2025-04-26 23:52:38 +0800

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.31%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.c
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.c
@@ -4,9 +4,12 @@ bool uniqueOccurrences(int* arr, int arrSize) {
     for(int i=0; i<arrSize; i++){
         hashtable[arr[i]+1000]++;
     }
-    for(int i=0; i<2001; i++){
-        if(hashtable[i] && countTable[hashtable[i]]++)
-            return false;
+    for (int i=0; i<2001; i++) {
+        int f = hashtable[i];
+        if(f){
+            if(countTable[f]) return false;
+            countTable[f] = 1;
+        }
     }
     return true;
 }
